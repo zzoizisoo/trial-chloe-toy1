d0dfc93cdfab48d4150ded1957d3b1b038181528
Chloe <zzoizisoo@gmail.com>
2024-08-23 00:29:44 +0900

    revived... by skipping update

--- imports/ui/components/ChatRoom/ChatsUseTracker.jsx
+++ imports/ui/components/ChatRoom/ChatsUseTracker.jsx
@@ -7,21 +7,19 @@ import { ChatsCollection } from "../../../api/chats/collection";
 
 // BUG: load more 하면 스크롤이 가장 밑으로 이동.
 export default () => {
-    const user = useTracker(() => Meteor.user());
+    const user = useTracker(() => Meteor.user(),[]);
     const PAGINATION_SIZE = 5;
     const [pageLength, setPageLength] = useState(PAGINATION_SIZE)
 
+    // CHATS
     const isChatLoading = useSubscribe('chatLogs', pageLength)
+    const chats = useTracker(() => ChatsCollection.find({}, { sort: { createdAt: -1 } }).fetch()
+    ,[pageLength], (prev, next)=> prev.length > next.length)
 
-    const chats = useTracker(() => {
-        return ChatsCollection.find({}, { sort: { createdAt: -1 } }).fetch();
-    },[pageLength])
 
+    // PROFILES
     const chatUids = [...new Set(chats.map(c => c.createdBy))]
-
     const isUserLoading = useSubscribe('usersProfileImgs', chatUids)
-
-    // Chat item에 사용자 프로필 이미지 추가
     const users = useTracker(() => {
         return Meteor.users.find({ _id: { $in: chatUids } }, {
             fields: {
@@ -31,15 +29,13 @@ export default () => {
     })
 
     const result = useTracker(()=>{ 
-        if(!isChatLoading() && !isUserLoading()){ 
             return chats.map(chat => {
                 const imgUrl = users.find((user) => user._id === chat.createdBy)?.profile?.profileImgUrl
                 return imgUrl ? { ...chat, profileImgUrl: imgUrl } : chat
             })
-        } else return [];
     })
     
-    // console.log('result ', result)
+    console.log('result ', result)
 
     return <div style={{ display: 'flex', flex: '1.5 1 0' }}>
         {user 
