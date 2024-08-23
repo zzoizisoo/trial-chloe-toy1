d997c95081b1c8695bbee094c87419d743abe120
Chloe <zzoizisoo@gmail.com>
2024-08-23 11:52:04 +0900

    split <Chats />

---
+++ imports/ui/components/ChatRoom/Chats.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react"
+import { useTracker, useSubscribe } from 'meteor/react-meteor-data';
+import InputArea from "./InputArea"
+import MyChatItem from "./MyChatItem"
+import OtherChatItem from "./OtherChatItem"
+import { ChatsCollection } from "../../../api/chats/collection";
+import Chats from "./Chats";
+export default () => {
+    const user = useTracker(() => Meteor.user(),[]);
+    const PAGINATION_SIZE = 5;
+    const [pageLength, setPageLength] = useState(PAGINATION_SIZE)
+
+    // CHATS
+    const isChatLoading = useSubscribe('chatLogs', pageLength)
+    const chats = useTracker(() => ChatsCollection.find({}, { sort: { createdAt: -1 } }).fetch()
+        , [pageLength], (prev, next) => prev.length > next.length)
+
+
+    // PROFILES
+    const chatUids = [...new Set(chats.map(c => c.createdBy))]
+    const isUserLoading = useSubscribe('usersProfileImgs', chatUids)
+    const users = useTracker(() => {
+        return Meteor.users.find({ _id: { $in: chatUids } }, {
+            fields: {
+                'profile.profileImgUrl': 1
+            }
+        }).fetch();
+    })
+
+
+    const result = useTracker(() => {
+        return chats.map(chat => {
+            const imgUrl = users.find((user) => user._id === chat.createdBy)?.profile?.profileImgUrl
+            return imgUrl ? { ...chat, profileImgUrl: imgUrl } : chat
+        })
+    })
+
+    return <div style={{ display: 'flex', flexDirection: "column-reverse", overflow: 'auto' }}>
+
+        {result && result.map(chat => chat.createdBy === user._id
+            ? <MyChatItem key={chat._id} chat={chat} imgUrl={chat.profileImgUrl} />
+            : <OtherChatItem key={chat._id} chat={chat} imgUrl={chat.profileImgUrl} />)}
+        <button onClick={() => setPageLength(pageLength + PAGINATION_SIZE)}>Load more</button>
+
+    </div>
+
+}
--- imports/ui/components/ChatRoom/ChatsUseTracker.jsx
+++ imports/ui/components/ChatRoom/ChatsUseTracker.jsx
@@ -4,50 +4,16 @@ import InputArea from "./InputArea"
 import MyChatItem from "./MyChatItem"
 import OtherChatItem from "./OtherChatItem"
 import { ChatsCollection } from "../../../api/chats/collection";
+import Chats from "./Chats";
 
 // BUG: load more 하면 스크롤이 가장 밑으로 이동.
 export default () => {
     const user = useTracker(() => Meteor.user(),[]);
-    const PAGINATION_SIZE = 5;
-    const [pageLength, setPageLength] = useState(PAGINATION_SIZE)
-
-    // CHATS
-    const isChatLoading = useSubscribe('chatLogs', pageLength)
-    const chats = useTracker(() => ChatsCollection.find({}, { sort: { createdAt: -1 } }).fetch()
-    ,[pageLength], (prev, next)=> prev.length > next.length)
-
-
-    // PROFILES
-    const chatUids = [...new Set(chats.map(c => c.createdBy))]
-    const isUserLoading = useSubscribe('usersProfileImgs', chatUids)
-    const users = useTracker(() => {
-        return Meteor.users.find({ _id: { $in: chatUids } }, {
-            fields: {
-                'profile.profileImgUrl': 1
-            }
-        }).fetch();
-    })
-
-    const result = useTracker(()=>{ 
-            return chats.map(chat => {
-                const imgUrl = users.find((user) => user._id === chat.createdBy)?.profile?.profileImgUrl
-                return imgUrl ? { ...chat, profileImgUrl: imgUrl } : chat
-            })
-    })
     
-    console.log('result ', result)
-
     return <div style={{ display: 'flex', flex: '1.5 1 0' }}>
         {user 
             ? <div style={{ flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'flex-end' }}>
-                <div style={{ display: 'flex', flexDirection: "column-reverse", overflow: 'auto' }}>
-
-                    { result && result.map(chat => chat.createdBy === user._id
-                        ? <MyChatItem key={chat._id} chat={chat} imgUrl={chat.profileImgUrl} />
-                        : <OtherChatItem key={chat._id} chat={chat} imgUrl={chat.profileImgUrl} />)}
-                    <button onClick={() => setPageLength(pageLength + PAGINATION_SIZE)}>Load more</button>
-
-                </div>
+                <Chats />
                 <InputArea />
             </div>
             :
