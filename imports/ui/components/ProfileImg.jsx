8a0a9c54dc23a26447dd822e5b75f79f60f0e4be
Chloe <zzoizisoo@gmail.com>
2024-09-05 12:09:05 +0900

    profileImg

--- imports/ui/components/ProfileImg.jsx
+++ imports/ui/components/ProfileImg.jsx
@@ -2,8 +2,13 @@ import React from "react"
 
 export default ({src, size})=> {
     return <img 
-            style={{display: "inline-block", width:size || 30, height: size || 30}}
-            src={src}
+            style={{
+                display: "inline-block", 
+                width:size || 30, 
+                height: size || 30,
+                borderRadius: '50%'
+            }}
+            src={src || '/profile.png'}
             />
     
 }
