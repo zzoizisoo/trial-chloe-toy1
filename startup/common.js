0e9112ab072de524f8c64bb24372ed8e8104d5fb
Chloe <zzoizisoo@gmail.com>
2024-09-05 12:56:18 +0900

    shcema error transform when ddp error

--- startup/common.js
+++ startup/common.js
@@ -1,7 +1,16 @@
+import SimpleSchema from "meteor/aldeed:simple-schema";
 import '../imports/api/users/methods'
 import '../imports/api/chats/methods'
 import '../imports/api/posts/methods'
 import '../imports/api/comments/methods'
 import '../imports/api/userFavorPosts/methods'
 
+
+//CROSS-SCHEMA CONCERN. 어디둬야하지 🤔
+SimpleSchema.defineValidationErrorTransform((error) => {
+    const ddpError = new Meteor.Error(error.message);
+    ddpError.error = "validation-error";
+    ddpError.details = error.details;
+    return ddpError;
+  });
 
